4f106a0cd4cd5f920a13d391ed264658a06241f9
Christopher Neugebauer <_@chrisjrn.com>
2016-06-27 08:01:42 +1000

    Templates (finished?) (#22)
    * Adds inset_illustration as a drop-down of svg data rather than as an uploady thing.
    
    * Adds external link types to the homepage models
    
    * Migrations
    
    * Initial site template with our new shiny templates
    
    * Moves all lca2017-specific assets to lca2017 subdirectory; symlinks from dist
    
    * Reflects changes in site_base
    
    * Deletes most of new assets in dist/
    
    * WIP home page
    
    * Adds external link list support
    
    * Adds white-right panel type to the home page
    
    * Removes an oops
    
    * Adds copyright text to the site.
    
    * Updates the remaining illustrations to be more compact.
    
    * Adds illustration field to content_page, and adds fields into the news page.
    
    * News index page
    
    * News page is done!
    
    * Formats the dates properly
    
    * Commits migrations
    
    * Optimises SVG files
    
    * better logo
    
    * oops
    
    * Removes margin on logo.
    
    * Optimises svgs, and uses tania’s originals
    
    * One more

--- cms_pages/models.py
+++ cms_pages/models.py
@@ -18,83 +18,85 @@ from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
 from wagtail.wagtailsearch import index
 
 
-class HomePage(Page):
+ILLUSTRATION_ANTARCTICA = "antarctica.svg"
+ILLUSTRATION_BRIDGE = "bridge.svg"
+ILLUSTRATION_CASINO = "casino.svg"
+ILLUSTRATION_CRADLE = "cradle.svg"
+ILLUSTRATION_DEVIL = "devil.svg"
+ILLUSTRATION_FALLS = "falls.svg"
+ILLUSTRATION_HOBART = "hobart.svg"
+ILLUSTRATION_LAVENDER = "lavender.svg"
+ILLUSTRATION_TUZ = "tuz.svg"
+ILLUSTRATION_WINEGLASS = "wineglass.svg"
 
-    BASIC_CONTENT_BLUE_LEFT = 1
-    BASIC_CONTENT_WHITE_RIGHT = 2
-    BASIC_CONTENT_TYPES = (
-        (BASIC_CONTENT_BLUE_LEFT, "Left-aligned image, blue-filtered image BG"),
-        (BASIC_CONTENT_WHITE_RIGHT, "Right-aligned image, white background"),
+ILLUSTRATION_TYPES = (
+    (ILLUSTRATION_ANTARCTICA, "Antarctica"),
+    (ILLUSTRATION_BRIDGE, "Bridge"),
+    (ILLUSTRATION_CASINO, "Casino"),
+    (ILLUSTRATION_CRADLE, "Cradle Mountain"),
+    (ILLUSTRATION_DEVIL, "Tasmanian Devil"),
+    (ILLUSTRATION_FALLS, "Waterfall"),
+    (ILLUSTRATION_HOBART, "Hobart"),
+    (ILLUSTRATION_LAVENDER, "Lavender"),
+    (ILLUSTRATION_TUZ, "Tuz"),
+    (ILLUSTRATION_WINEGLASS, "Wineglass"),
+)
+
+
+class BasicContentBlock(blocks.StructBlock):
+
+    class Meta:
+        template = "cms_pages/home_page_blocks/basic_content.html"
+
+    PANEL_BLUE_LEFT = "blue_left"
+    PANEL_WHITE_RIGHT = "white_right"
+    PANEL_TYPES = (
+        (PANEL_BLUE_LEFT, "Left-aligned image, blue-filtered image BG"),
+        (PANEL_WHITE_RIGHT, "Right-aligned image, white background"),
     )
 
-    ILLUSTRATION_ANTARCTICA = "antarctica.svg"
-    ILLUSTRATION_BRIDGE = "bridge.svg"
-    ILLUSTRATION_CASINO = "casino.svg"
-    ILLUSTRATION_CRADLE = "cradle.svg"
-    ILLUSTRATION_DEVIL = "devil.svg"
-    ILLUSTRATION_FALLS = "falls.svg"
-    ILLUSTRATION_HOBART = "hobart.svg"
-    ILLUSTRATION_LAVENDER = "lavender.svg"
-    ILLUSTRATION_TUZ = "tuz.svg"
-    ILLUSTRATION_WINEGLASS = "wineglass.svg"
-
-    ILLUSTRATION_TYPES = (
-        (ILLUSTRATION_ANTARCTICA, "Antarctica"),
-        (ILLUSTRATION_BRIDGE, "Bridge"),
-        (ILLUSTRATION_CASINO, "Casino"),
-        (ILLUSTRATION_CRADLE, "Cradle Mountain"),
-        (ILLUSTRATION_DEVIL, "Tasmanian Devil"),
-        (ILLUSTRATION_FALLS, "Waterfall"),
-        (ILLUSTRATION_HOBART, "Hobart"),
-        (ILLUSTRATION_LAVENDER, "Lavender"),
-        (ILLUSTRATION_TUZ, "Tuz"),
-        (ILLUSTRATION_WINEGLASS, "Wineglass"),
-    )
-
-    EXTERNAL_LINK_TWITTER = "twitter.svg"
-    EXTERNAL_LINK_FACEBOOK = "facebook.svg"
-    EXTERNAL_LINK_LINKEDIN = "linkedin.svg"
-    EXTERNAL_LINK_GENERIC = "generic.svg"
+    EXTERNAL_LINK_TWITTER = "twitter"
+    EXTERNAL_LINK_GENERIC = "generic"
 
     EXTERNAL_LINK_TYPES = (
         (EXTERNAL_LINK_TWITTER, "Twitter"),
-        (EXTERNAL_LINK_FACEBOOK, "Facebook"),
-        (EXTERNAL_LINK_LINKEDIN, "LinkedIn"),
         (EXTERNAL_LINK_GENERIC, "Generic URL"),
     )
 
+    panel_type = blocks.ChoiceBlock(
+        choices=PANEL_TYPES,
+        required=True,
+    )
+    heading = blocks.CharBlock(required=True)
+    inset_illustration = blocks.ChoiceBlock(
+        choices=ILLUSTRATION_TYPES,
+        required=True,
+    )
+    background_image = imageblocks.ImageChooserBlock(
+        required=False,
+        help_text="This is used as the background image of a "
+                  "blue-left block. It's not used for white-right."
+    )
+    body = blocks.RichTextBlock(required=True)
+    link = blocks.StructBlock([
+        ("page", blocks.PageChooserBlock()),
+        ("title", blocks.CharBlock(required=True)),
+    ])
+    external_links = blocks.ListBlock(
+        blocks.StructBlock([
+            ("alt", blocks.CharBlock(required=True)),
+            ("icon", blocks.ChoiceBlock(
+                choices=EXTERNAL_LINK_TYPES,
+                required=True,
+            )),
+            ("url", blocks.URLBlock(required=True)
+        )])
+    )
+
+class HomePage(Page):
+
     body = StreamField([
-        ("basic_content", blocks.StructBlock([
-            ("type", blocks.ChoiceBlock(
-                choices=BASIC_CONTENT_TYPES,
-                required=True,
-            )),
-            ("heading", blocks.CharBlock(required=True)),
-            ("inset_illustration", blocks.ChoiceBlock(
-                choices=ILLUSTRATION_TYPES,
-                required=True,
-            )),
-            ("background_image", imageblocks.ImageChooserBlock(
-                required=False,
-                help_text="This is used as the background image of a "
-                          "blue-left block. It's not used for white-right."
-            )),
-            ("body", blocks.RichTextBlock(required=True)),
-            ("link", blocks.StructBlock([
-                ("page", blocks.PageChooserBlock()),
-                ("title", blocks.CharBlock(required=True)),
-            ])),
-            ("external_links", blocks.ListBlock(
-                blocks.StructBlock([
-                    ("alt", blocks.CharBlock(required=True)),
-                    ("icon", blocks.ChoiceBlock(
-                        choices=EXTERNAL_LINK_TYPES,
-                        required=True,
-                    )),
-                    ("url", blocks.URLBlock(required=True)
-                )])
-            )),
-        ])),
+        ("basic_content", BasicContentBlock()),
         # TODO: keynotes
         # TODO: other bits
     ])
@@ -112,7 +114,7 @@ class AbstractContentPage(Page):
 
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-    main_image = models.ForeignKey(
+    background_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
         blank=True,
@@ -126,7 +128,7 @@ class AbstractContentPage(Page):
     ]
 
     content_panels = Page.content_panels + [
-        ImageChooserPanel('main_image'),
+        ImageChooserPanel('background_image'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full")
     ]
@@ -134,45 +136,41 @@ class AbstractContentPage(Page):
 
 class ContentPage(AbstractContentPage):
 
-    IMAGE_DISPLAY_FEATURE = 1
-    IMAGE_DISPLAY_VIGNETTE = 2
-
-    IMAGE_DISPLAY = (
-        (IMAGE_DISPLAY_FEATURE, "Banner with background feature image"),
-        (IMAGE_DISPLAY_VIGNETTE, "No banner, circular vignette"),
+    inset_illustration = models.CharField(
+        choices=ILLUSTRATION_TYPES,
+        max_length=256,
     )
 
-    image_display = models.IntegerField(
-        choices=IMAGE_DISPLAY,
-    )
-
-    def image_display_feature(self):
-        return self.image_display == IMAGE_DISPLAY_FEATURE
-
-    def image_display_vignette(self):
-        return self.image_display == IMAGE_DISPLAY_VIGNETTE
-
-    content_panels = [
-        FieldPanel('image_display')
-    ] + AbstractContentPage.content_panels
+    content_panels = AbstractContentPage.content_panels + [
+        FieldPanel('inset_illustration')
+    ]
 
 
 # News pages
 
-class NewsIndexPage(Page):
-    intro = RichTextField(blank=True)
+class NewsIndexPage(AbstractContentPage):
+
+    def child_pages(self):
+        return NewsPage.objects.live().child_of(self).specific().order_by("-date")
 
     subpage_types = [
         "NewsPage",
     ]
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full"),
-    ]
+    content_panels = AbstractContentPage.content_panels
 
 
 class NewsPage(AbstractContentPage):
+
     date = models.DateField("Post date")
+
+    portrait_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     parent_page_types = [
         NewsIndexPage,
@@ -180,4 +178,5 @@ class NewsPage(AbstractContentPage):
 
     content_panels = AbstractContentPage.content_panels + [
         FieldPanel('date'),
+        ImageChooserPanel('portrait_image'),
     ]
