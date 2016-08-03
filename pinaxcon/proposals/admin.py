504cf775744a1611551c27f79981739c1aece0bb
Christopher Neugebauer <_@chrisjrn.com>
2016-08-04 09:59:50 +1000

    Allows us to manage additional speakers from the admin (#58)
    * Allows us to manage additional speakers from the admin
    
    * Show speaker e-mail addresses. Fixes #57
    
    * Denotes fields as required.

--- pinaxcon/proposals/admin.py
+++ pinaxcon/proposals/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
 import models
+from symposion.proposals import models as symposion_models
 
-admin.site.register(models.TalkProposal)
-admin.site.register(models.TutorialProposal)
-admin.site.register(models.MiniconfProposal)
+
+@admin.register(models.TalkProposal)
+@admin.register(models.TutorialProposal)
+@admin.register(models.MiniconfProposal)
+class CategoryAdmin(admin.ModelAdmin):
+
+    class AdditionalSpeakerInline(admin.TabularInline):
+        model = symposion_models.AdditionalSpeaker
+
+    inlines = [
+        AdditionalSpeakerInline,
+    ]
