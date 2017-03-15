6f52df7581bca0413e68012b65cbd7953bacbc01
Clinton Roy <clinton.roy@gmail.com>
2017-03-15 19:34:05 +1000

    added text as requested by paper team.

--- pinaxcon/proposals/models.py
+++ pinaxcon/proposals/models.py
@@ -53,7 +53,10 @@ class PyConAuProposal(Proposal):
 
     area = models.ManyToManyField(
         Section,
-        help_text="Please select the areas of the conference that you think your talk is applicable to. The paper committee may ask you to reconsider your desired areas.")
+        help_text="Please select the areas of the conference that you think your talk is applicable to. "
+                  "Allowing us to place your talk in any track will give the greatest likelihood of your talk being accepted and "
+                  "also allows us to design the best possible program schedule. "
+                  "The paper committee may ask you to reconsider your desired areas.")
 
     LENGTH_SHORT = 1
     LENGTH_LONG = 2
