e72ab520fe61843d28b6a4286a1a77acd39a277c
Clinton Roy <clinton.roy@gmail.com>
2017-06-02 20:47:31 +1000

    add a cap for each day of childcare, just in case.

--- pinaxcon/registrasion/management/commands/populate_inventory.py
+++ pinaxcon/registrasion/management/commands/populate_inventory.py
@@ -528,6 +528,35 @@ class Command(BaseCommand):
         )
         sprint_tuesday_cap.products.set([self.sprint_ticket_tuesday])
 
+        childcare_cap = 20
+
+        childcare_friday_cap = self.find_or_make(
+            cond.TimeOrStockLimitFlag,
+            ("description", ),
+            description="Friday childcare cap",
+            condition=cond.FlagBase.DISABLE_IF_FALSE,
+            limit=childcare_cap,
+        )
+        childcare_friday_cap.products.set([self.childcare_friday])
+
+        childcare_saturday_cap = self.find_or_make(
+            cond.TimeOrStockLimitFlag,
+            ("description", ),
+            description="Saturday childcare cap",
+            condition=cond.FlagBase.DISABLE_IF_FALSE,
+            limit=childcare_cap,
+        )
+        childcare_saturday_cap.products.set([self.childcare_saturday])
+
+        childcare_sunday_cap = self.find_or_make(
+            cond.TimeOrStockLimitFlag,
+            ("description", ),
+            description="Sunday childcare cap",
+            condition=cond.FlagBase.DISABLE_IF_FALSE,
+            limit=childcare_cap,
+        )
+        childcare_sunday_cap.products.set([self.childcare_sunday])
+
         # Volunteer tickets are for volunteers only
         volunteers = self.find_or_make(
             cond.GroupMemberFlag,
