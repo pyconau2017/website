2140940c3e16fdf5c82a62ca393ec684c152da9c
Nick Seidenman <seidenman@wehi.edu.au>
2017-08-12 15:39:37 +1000

    Added column switches.  These are handy if the CSV layout ever changes.

--- pinaxcon/management/commands/update_video_urls.py
+++ pinaxcon/management/commands/update_video_urls.py
@@ -38,15 +38,28 @@ class Command(BaseCommand):
     help = """\
     Update the video_url property in the Presentation instances found
     in the CSV file supplied on the command line.
+
+    Default columns within the CSV are 4 and 5.  These can be changed using
+    the --pr-col and --yt-col switches.  The offset from the end of the
+    presentation URL for the presentation's pk can be set using the --pkndx
+    switch.  IT IS INADVISABLE TO CHANGE THESE UNLESS YOU KNOW WHAT YOU ARE
+    DOING!  YOU HAVE BEEN WARNED!
     """
 
     def add_arguments(self, parser):
         parser.add_argument('--empty-only', action='store_true', default=False,
                             help='Only update Presentations with blank video_url property.')
+        parser.add_argument('--pr-col', type=int, default=4,
+                            help='Column (from 0) of presentation URL. [4]')
+        parser.add_argument('--yt-col', type=int, default=5,
+                            help='Column (fron 0) of youtube URL. [5]')
+        parser.add_argument('--pkndx', type=int, default=2,
+                            help='Column (fron 0) of youtube URL. [5]')
         parser.add_argument('av_updates', nargs='*', type=str)
 
     def handle(self, *args, **options):
 
-        vu = VideoURLUpdater(options['av_updates'][0])
+        vu = VideoURLUpdater(options['av_updates'][0],  pr_col=options['pr_col'],
+                  yt_col=options['yt_col'], pkndx=options['pkndx'])
         vu.update(empty_only=options['empty_only'])
         print("{} presentations updated".format(len(vu)))
